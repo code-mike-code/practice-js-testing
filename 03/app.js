f7d66d41df67a2b3c898f6e80732f83ae6d05b22
Michał <m.majewski.dm@gmail.com>
2025-05-16 22:56:27 +0200

    added number validation, calc logic and group test cases

--- 03/app.js
+++ 03/app.js
@@ -1,10 +1,20 @@
 export default function randomNumber(min, max) {
-    if(typeof min !== 'number' || typeof max !== 'number') {
-        throw new TypeError('arguments must be numbers')
-    }
+    randomNumberValidation(min, max)
+
     if(min === max) {
         return min
     }
+
+    return calcRandomNumber()
+}
+
+function randomNumberValidation(min, max){
+    if(typeof min !== 'number' || typeof max !== 'number') {
+        throw new Error('arguments must be numbers')
+    }
+}
+
+function calcRandomNumber() {
     const range = max - min + 1
     return Math.floor(Math.random() * range) + min
 }
--- 03/app.test.js
+++ 03/app.test.js
@@ -1,12 +1,18 @@
 import randomNumber from './app';
 
 describe('randomNumber', () => {
-    it('should return 1 when range is from 1 to 1', () => {
-        expect(randomNumber(1, 1)).toBe(1)
+
+    describe('rangeTest', () => {
+        it('should return 1 when range is from 1 to 1', () => {
+            expect(randomNumber(1, 1)).toBe(1)
+        })
     })
-    it('should throw TypeError when arguments are not numbers', () => {
-        expect(() => randomNumber('a', 5)).toThrow(TypeError)
-        expect(() => randomNumber(1, 'b')).toThrow(TypeError)
-        expect(() => randomNumber(undefined, 5)).toThrow(TypeError)
+
+    describe('numberValidation', () => {
+        it('should throw TypeError when arguments are not numbers', () => {
+            expect(() => randomNumber('a', 4)).toThrow(TypeError)
+            expect(() => randomNumber(3, 'b')).toThrow(TypeError)
+            expect(() => randomNumber(undefined, 4)).toThrow(TypeError)
+        })
     })
 })
