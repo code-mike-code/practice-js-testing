5a73efcff6ac4f0fd0f1cc19273f164874338b18
Michał <m.majewski.dm@gmail.com>
2025-05-16 08:53:32 +0200

    first test, task 03

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,1 +1,7 @@
 import randomNumber from './app';
+
+describe('randomNumber', () => {
+    it('should return 1 when range is from 1 to 1', () => {
+        expect(randomNumber(1, 1)).toBe(1)
+    })
+})
