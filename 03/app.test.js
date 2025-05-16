73f7250f7d1905142ab6193aeb4158c8976300b5
Michał <m.majewski.dm@gmail.com>
2025-05-16 09:40:15 +0200

    add test to check if argument is a number

--- 03/app.test.js
+++ 03/app.test.js
@@ -4,4 +4,9 @@ describe('randomNumber', () => {
     it('should return 1 when range is from 1 to 1', () => {
         expect(randomNumber(1, 1)).toBe(1)
     })
+    it('should throw TypeError when arguments are not numbers', () => {
+        expect(() => randomNumber('a', 5)).toThrow(TypeError)
+        expect(() => randomNumber(1, 'b')).toThrow(TypeError)
+        expect(() => randomNumber(undefined, 5)).toThrow(TypeError)
+    })
 })
