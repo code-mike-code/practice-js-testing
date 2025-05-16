ed504bfc173f246fe4b0e30bc4050d2a70d991ea
Michał <m.majewski.dm@gmail.com>
2025-05-16 09:42:56 +0200

    add type validation for randomNumber func argument

--- 03/app.js
+++ 03/app.js
@@ -1,4 +1,7 @@
 export default function randomNumber(min, max) {
+    if(typeof min !== 'number' || typeof max !== 'number') {
+        throw new TypeError('arguments must be numbers')
+    }
     if(min === max) {
         return min
     }
