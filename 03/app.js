c6e1be2705eeebca3cef6fd2899248d553945070
Michał <m.majewski.dm@gmail.com>
2025-05-16 10:13:01 +0200

    added varible "range"

--- 03/app.js
+++ 03/app.js
@@ -5,5 +5,6 @@ export default function randomNumber(min, max) {
     if(min === max) {
         return min
     }
-    return Math.floor(Math.random() * (max - min + 1)) + min
+    const range = max - min + 1
+    return Math.floor(Math.random() * range) + min
 }
