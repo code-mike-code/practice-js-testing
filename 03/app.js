8885bbc6fd944771cf36204d81de667a5ed32c2e
Michał <m.majewski.dm@gmail.com>
2025-05-16 09:33:27 +0200

    refactor function randomNumber

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,6 @@
 export default function randomNumber(min, max) {
-
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-
+    if(min === max) {
+        return min
+    }
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
