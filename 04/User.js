cee078a238a76f17d45a0848e5bc23639323b780
Michał <m.majewski.dm@gmail.com>
2025-05-19 20:33:49 +0200

    added password validation and isPasswordValid method

--- 04/User.js
+++ 04/User.js
@@ -1,11 +1,18 @@
 export default class User {
     constructor( {email, password} ) {
         if (!this.isEmailValid(email)) {
-            throw new Error('Invalid emil format')
+            throw new Error('Invalid emil format!')
+        }
+        if (!this.isPasswordValid(password)) {
+            throw new Error('Password must be at least 6 characters long!')
         }
 
         this.email = email
         this.password = password
+    }
+
+    isPasswordValid(password) {
+        return password.length >= 6
     }
 
     isEmailValid(email) {
@@ -15,7 +22,7 @@ export default class User {
     getEmail() {
         return this.email
     }
-
+    
     getPassword() {
         return this.password
     }
